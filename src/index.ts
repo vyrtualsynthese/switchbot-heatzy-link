7f6feddade01b41ae991fd9c5e76f7b7abaf6b52
Valentin RONTEIX <valentin.ronteix@ashudev.com>
2021-10-12 22:30:41 +0200

    :sparkles: implement heatzy auth

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+require('dotenv').config();
+const axios = require('axios').default;
+
+axios.post('https://euapi.gizwits.com/app/login', {
+    username: process.env.HEATZY_USERNAME,
+    password: process.env.HEATZY_PASSWORD
+    }, {
+    headers: {
+        'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3'
+    },
+    })
+    .then(function (response: any) {
+        console.log(response.data);
+    })
+    .catch(function (error: any) {
+        console.log(error);
+    });
